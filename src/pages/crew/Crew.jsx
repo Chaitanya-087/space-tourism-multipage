e9dea01da3e32eb41eea30de9023439376195016
chaitanya <sparkscj234@gmail.com>
2022-06-28 22:36:41 +0530

    tweeked

--- src/pages/crew/Crew.jsx
+++ src/pages/crew/Crew.jsx
@@ -1,33 +1,36 @@
 import React, { useState } from 'react'
 import './crew.css';
-const Crew = ({crewMembers}) => {
-  const [crewIdx, setCrewIdx]= useState(0)
-  return (
-    <div className="body">
-      <div className='crewContainer'>
-        <div className="details">
-          <div className="title">
-            <p className='title fs-s'><span className='crew-number fw-700'>01</span> PICK YOUR CREW</p>
-          </div>
-          <div className="name">
-            <p className='role'>{crewMembers[crewIdx].role}</p>
-            <p className="name">{crewMembers[crewIdx].name}</p>
-          </div>
-          <div className="crewDetails">
+const Crew = ({ crewMembers }) => {
+  const [crewIdx, setCrewIdx] = useState(0)
 
-          </div>
-          <div className="nav">
-            <ul className='nav-ul'>
-              {
-              crewMembers.map((people,index)=>(
-                <li key={index} onClick={()=>setCrewIdx(index)} className='navigation' ><p>{people.name}</p></li>
-              ))
-              }
-            </ul>
-          </div>
+  return (
+    <div className="crew">
+      <div className='crewContainer'>
+        <div className="crewHeading">
+          <p className='title fs-mb-s fw-400 '><span className='crew-number fw-700 fs-mb-s' style={{ color: 'grey' }}>02</span> PICK YOUR CREW</p>
         </div>
-        <div className="imageWrapper">
-          <img className="crew-image" src={crewMembers[crewIdx].image} alt="image" width='80%' height='80%'/>
+        <div className='crewDetailsWrapper'>
+          <div className="details">
+            <div className="crewInfo">
+              <p className='role text-mb-center fs-mb-16 fs-s text-uppercase fw-400 belle' style={{ opacity: '0.5',letterSpacing:'1.2px' }}>{crewMembers[crewIdx].role}</p>
+              <p className="name text-mb-center fs-lg fs-mb-s text-uppercase belle fw-400">{crewMembers[crewIdx].name}</p>
+              <div className="crewDetails text-mb-center" style={{ lineHeight: '1.2', letterSpacing: '1px' }}>
+                {crewMembers[crewIdx].bio}
+              </div>
+            </div>
+
+              <div className="blobs">
+                {
+                  crewMembers.map((mem, index) =>
+                    <div key={mem.name} className={`blob ${crewIdx === index ? 'active' : ''}`} onClick={() => setCrewIdx(index)}></div>
+                  )
+                }
+              </div>
+          </div>
+          <div className="imageWrapper">
+            <img className="crew-image" src={crewMembers[crewIdx].image} alt="crew" width='80%' height='80%' />
+          </div>
+
         </div>
       </div>
     </div>
